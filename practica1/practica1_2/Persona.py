8710a0bec6dd9deffea392d19df2304bdc8ddfc2
Ramon Andres Ocegueda Montoya <andresocegueda1.d@gmail.com>
2025-02-09 13:03:23 -0800

    practica 1

---
+++ practica1/practica1_2/Persona.py
@@ -0,0 +1,19 @@
+class persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    
+    def cumpleanos(self):
+        self.edad += 1
+
+    def __str__(self):
+        return "Nombre: " + self.nombre + ", Edad: " + str(self.edad)
+    
+primerpersona = persona("Juan", 25)
+
+print(primerpersona)
+
+primerpersona.cumpleanos()
+
+print(primerpersona)
+
